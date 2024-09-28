61dfc6d012229bdc026c1a958f7dba39d41de15c
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-29 00:42:16 +0530

    Add files via upload

---
+++ Java_Code's/242.Valid Anagram.java
@@ -0,0 +1,17 @@
+//242. Valid Anagram
+import java.util.Arrays; 
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int k =0;
+        if(s.length()!=t.length())
+        {
+            return false ; 
+        }
+        char[] sArray=s.toCharArray(); 
+        char[] tArray=t.toCharArray(); 
+        Arrays.sort(sArray); 
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray,tArray);
+        
+    }
+}
