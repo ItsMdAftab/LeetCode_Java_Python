9c1e0de1c09955f9d4712c36daa0717ce136af6a
Muhammad Aftab <mdaftab01@outlook.com>
2025-05-30 11:21:37 +0530

    Time: 10 ms (81.87%), Space: 48.4 MB (83.82%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,24 @@
+import java.util.Arrays; 
+class Solution {
+    public int findContentChildren(int[] g, int[] s) 
+    {
+        Arrays.sort(g); 
+        Arrays.sort(s); 
+        int i=0; 
+        int j=0; 
+        int content=0; 
+        while(i<g.length&&j<s.length)
+        {
+            if(g[i]<=s[j])
+            {
+                content++; 
+                i++; 
+            }
+           
+                j++; 
+            
+
+        }  
+        return content;    
+    }
+}
